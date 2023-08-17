080e90496648a3c52d8b73e8558816f1fead1ea6
Umut Erozan <umut_erozan@hotmail.com>
2023-08-17 18:22:56 +0300

    slider section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import Header from "@/components/common/Header";
 import Navbar from "@/components/common/Navbar";
+import Slider from "@/components/home/Slider";
 
 export default function HomePage() {
   return (
     <>
       <Header />
       <Navbar />
+      <Slider />
     </>
   )
 }
