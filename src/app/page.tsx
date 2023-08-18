477b3869e94973c0f531f5637c1ce90e8bbbc089
Umut Erozan <umut_erozan@hotmail.com>
2023-08-18 18:55:49 +0300

    recentpostsection 50%

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import Header from "@/components/common/Header";
 import Navbar from "@/components/common/Navbar";
+import RecentPostSection from "@/components/home/RecentPostSection";
 import Slider from "@/components/home/Slider";
 
 export default function HomePage() {
@@ -8,6 +9,7 @@ export default function HomePage() {
       <Header />
       <Navbar />
       <Slider />
+      <RecentPostSection />
     </>
   )
 }
